f44c738cc8b9f45e2700bf2ebaa8517669c55d2e
Rajiv Bhardwaj <rajiv.bhardwaj@afourtech.com>
2017-05-18 10:52:18 +0530

    making side-menu common/reusable for all pages

---
+++ src/pages/side-menu/side-menu.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Logger } from '../../utility/Logger';
+
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: 'side-menu.html',
+})
+export class SideMenuPage {
+
+ constructor(private logger: Logger) {
+    
+  }
+
+}
