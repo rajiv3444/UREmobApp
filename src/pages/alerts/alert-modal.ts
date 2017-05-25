300684b60bd2acd9e84da928144187842253876e
Rajiv Bhardwaj <rajiv.bhardwaj@afourtech.com>
2017-05-25 12:27:16 +0530

    Anushruti: popup modal page implementation for alert page

---
+++ src/pages/alerts/alert-modal.ts
@@ -0,0 +1,21 @@
+
+import { Component, OnInit } from '@angular/core';
+import { NavParams, ViewController } from 'ionic-angular';
+
+@Component({
+    templateUrl: 'alert-modal.html'
+})
+export class AlertsModalPage implements OnInit {
+    pageTitle = 'Description'
+    displayData: any[] = [];        
+    constructor(private navParams: NavParams, public viewCtrl: ViewController) {
+    }
+
+    ngOnInit() {
+        this.displayData = this.navParams.get('alertItemsArray');
+    }
+
+    // DismissModalPage() {
+    //     this.viewCtrl.dismiss();
+    // }
+}
--- src/pages/alerts/alerts.ts
+++ src/pages/alerts/alerts.ts
@@ -1,24 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AlertsService } from '../../providers/alerts-service';
 import { Utils } from '../../utility/Utils';
 import { Logger } from '../../utility/Logger';
 import { GenericData } from '../../Models/common-models';
-import {  AlertController } from 'ionic-angular';
+import { AlertController, ModalController, ViewController } from 'ionic-angular';
+import { AlertsModalPage } from './alert-modal';
 
 @Component({
     selector: 'alerts',
     templateUrl: 'alerts.html'
-   // styles: ['.even { background-color: #ccc;}','.odd { background-color:#FFFFFF; }']
-   
+    // styles: ['.even { background-color: #ccc;}','.odd { background-color:#FFFFFF; }']
+
 })
-export class AlertsPage {
-    pageTitle:string = 'Alerts';
+export class AlertsPage implements OnInit {
+    pageTitle: string = 'Alerts';
     public displayAlerts: any[] = [];
-   
-    constructor(private alertsService: AlertsService, private utils: Utils, private logger: Logger,private alertController: AlertController) {
+
+    constructor(private alertsService: AlertsService, private utils: Utils, private logger: Logger, private alertController: AlertController, public modalCtrl: ModalController) {
+
+    }
+    ngOnInit() {
         this.GetAlerts();
     }
-
     GetAlerts() {
         this.alertsService.GetAlerts()
             .subscribe((resp) => {
@@ -29,21 +32,32 @@ export class AlertsPage {
                 this.logger.LogError('Erro while fetching alerts data');
             });
     }
-    AlertDetail(alertItems)  {
-         let row = "";
-       for (let key in alertItems) {
-      row += "<tr><td>" + key + "</td><td>" + alertItems[key] + "</td></tr>";
+
+    OpenModalPage(alertItem) {
+        let itemsArray: any[] = [];
+        let alertObj;
+        for (let key in alertItem) {
+            alertObj = { key: key, value: alertItem[key] };
+            itemsArray.push(alertObj)
+        }
+        let obj = { alertItemsArray: itemsArray };
+        let appModal = this.modalCtrl.create(AlertsModalPage, obj);
+        appModal.present();
     }
 
-    let alert = this.alertController.create({
-      title: 'alert - ' + alertItems.id,
-      subTitle: alertItems.created,
-      message: "<table border='1'><tr><th>Key</th><th> value</th></tr>" + row + "</table>",
-      buttons: ['Ok'],
-      cssClass: 'full-popup',      
-    });
-    alert.present();
-  }
-    
+    // AlertDetail(alertItem) {
+    //     let row = "";
+    //     for (let key in alertItem) {
+    //         row += "<tr><td>" + key + "</td><td>" + alertItem[key] + "</td></tr>";
+    //     }
 
+    //     let alert = this.alertController.create({
+    //         title: 'alert - ' + alertItem.id,
+    //         subTitle: alertItem.created,
+    //         message: "<table border='1'><tr><th>Key</th><th> value</th></tr>" + row + "</table>",
+    //         buttons: ['Ok'],
+    //         cssClass: 'full-popup',
+    //     });
+    //     alert.present();
+    // }
 }
